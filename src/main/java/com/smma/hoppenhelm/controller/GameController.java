9ccef009a10222b47bb3d9447a4ba6f0043c0ab1
sepehrmoh81 <sepehr.moh81@gmail.com>
2024-06-13 23:06:27 +0330

    Move animations - Dynamic children

--- src/main/java/com/smma/hoppenhelm/controller/GameController.java
+++ src/main/java/com/smma/hoppenhelm/controller/GameController.java
@@ -1,18 +1,24 @@
 package com.smma.hoppenhelm.controller;
 import com.smma.hoppenhelm.model.Player;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class GameController {
     @FXML private Label playerName;
     @FXML private Label score;
-    @FXML private HBox objectParent;
+    @FXML private VBox objectParent;
+    @FXML private HBox level0;
+    @FXML private HBox level1;
 
     private Player player;
-    private ImageView img;
+
     public void setPlayer(Player player) {
         this.player = player;
         this.playerName.setText(player.getName());
@@ -20,10 +26,15 @@ public class GameController {
     }
 
     @FXML
+    public void initialize() {
+        renderGame();
+    }
+
+    @FXML
     private void onMove() {
         player.move();
-
         updatePlayerInfo();
+        animateMove();
     }
 
     @FXML
@@ -38,5 +49,47 @@ public class GameController {
 
     private void updatePlayerInfo() {
         score.setText(String.valueOf(player.getX()));
+    }
+
+    private void renderGame(){
+        for(int i = 0 ; i < 4; i++){
+            Button button = new Button();
+            button.setPrefWidth(90);
+            button.setText("Button " + i);
+            level0.getChildren().add(button);
+        }
+        for(int i = 0 ; i < 4; i++){
+            Button button = new Button();
+            button.setPrefWidth(90);
+            button.setText("Button " + i);
+            level1.getChildren().add(button);
+        }
+    }
+
+    private void renderNextTile(){
+        Button button0 = new Button();
+        button0.setPrefWidth(90);
+        button0.setText("Button " + (player.getX() + 3));
+        level0.getChildren().add(button0);
+
+        Button button1 = new Button();
+        button1.setPrefWidth(90);
+        button1.setText("Button " + (player.getX() + 3));
+        level1.getChildren().add(button1);
+    }
+
+    private void animateMove(){
+        TranslateTransition transition = new TranslateTransition(Duration.millis(500));
+        transition.setByX(-90);
+        transition.setCycleCount(1);
+        transition.setAutoReverse(false);
+        transition.setNode(objectParent);
+        transition.play();
+        transition.setOnFinished(actionEvent -> {
+            level0.getChildren().remove(0);
+            level1.getChildren().remove(0);
+            objectParent.setTranslateX(0);
+            renderNextTile();
+        });
     }
 }
